70a76bcb3604d428797e6874e7b01d899e24bbde
Ankit Singh <ankitsinghniti@gmail.com>
2022-06-28 14:13:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/newsapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.newsapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MainListFragment mlf = new MainListFragment();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.frameLayout, mlf);
+        fragmentTransaction.commit();
+    }
+
+
+}
